128af3a79b58b794f8375679a4a9e65b81f5bb9a
Jarosław K <jarasss1@wp.pl>
2025-09-08 15:47:40 +0200

    refactor(form): Unify Interview state and improve component structure
    - Unifies the Statement component's state under the new allFormData structure.

--- src/Components/Step/Interview/Interview.tsx
+++ src/Components/Step/Interview/Interview.tsx
@@ -1,32 +1,16 @@
-import { useAppContext } from '../../../context/AppContext';
-import { Formik, Field } from 'formik';
+import { Field } from 'formik';
 import Style from './Interview.module.scss';
-import { FormNavigation } from '../../Section/FormNavigation/FormNavigation';
 
 const Interview = () => {
-  const appContext = useAppContext();
   return (
-    <Formik
-      initialValues={appContext.interview}
-      onSubmit={(value, { setSubmitting }) => {
-        setSubmitting(false);
-        appContext.setInterview(value);
-      }}
-    >
-      {({ handleSubmit, submitForm }) => (
-        <>
-          <form onSubmit={handleSubmit}>
-            <h2 className={Style.title}>I - WYWIAD</h2>
-            <Field
-              className={Style.text}
-              name="interviewDescription"
-              as="textarea"
-            />
-            <FormNavigation onSaveForm={submitForm} />
-          </form>
-        </>
-      )}
-    </Formik>
+    <>
+      <h2 className={Style.title}>I - WYWIAD</h2>
+      <Field
+        className={Style.text}
+        name="interview.interviewDescription"
+        as="textarea"
+      />
+    </>
   );
 };
 export default Interview;
